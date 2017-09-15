3f3d0a51ca3920c2c045e8b659a3bf66c448678b
Matthew Hunger <matt.hunger@gmail.com>
2017-09-15 11:00:53 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,10 +1,10 @@
 // declare your functions here...
 function paragraphSelector() {
-  return $('p')
+  return $('p');
 }
 
 function lastImageSelector() {
-  return $('div img:last-child')
+  return $('img:last');
 }
 
 function ninjaBabySelector() {
