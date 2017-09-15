8df81527baf7b3d30d3f0f0f984cb8517512752d
Matthew Hunger <matt.hunger@gmail.com>
2017-09-15 11:01:21 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -8,7 +8,7 @@ function lastImageSelector() {
 }
 
 function ninjaBabySelector() {
-  return $('#baby-ninja')
+  return $('#baby-ninja');
 }
 
 function divSelector() {
@@ -16,5 +16,5 @@ function divSelector() {
 }
 
 function firstListItem() {
-  return $('#pic-list img:first-child')
+  return $('ul#pic-list li:first-child')
 }
